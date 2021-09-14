b2fbffbd3a2faac395536bd9e1975995f3cd5807
mcdnmd <kpoltoradnev@gmail.com>
2021-09-14 13:15:13 +0500

    v0.1 change main page

--- app/api/views.py
+++ app/api/views.py
@@ -6,4 +6,8 @@ from api.models import Record
 
 def index(request):
     records = Record.objects.all()
-    return HttpResponse(f'<p>{len(records)}</p>')
+    result = '<h1>Records in DB</h1>'
+    for rec in records:
+        result += f'\n<h3>ID:{records.id}</h3>\n\t<p>{records.text}</p>'
+
+    return HttpResponse(result)
