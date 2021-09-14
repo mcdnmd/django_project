957fad4dac898c093084bd07995b549467d72736
mcdnmd <kpoltoradnev@gmail.com>
2021-09-14 14:25:55 +0500

    v0.5 Fix frontend

--- app/api/views.py
+++ app/api/views.py
@@ -7,8 +7,8 @@ from api.models import Record
 def index(request):
     records = Record.objects.all()
     result = '<h1>Records in DB</h1>'
-    
+
     for rec in records:
-        result += f'\n<h3>ID:{records.id}</h3>\n\t<p>{records.text}</p>'
+        result += f'\n<h3>ID:{rec.id}</h3>\n\t<p>{rec.text}</p>'
 
     return HttpResponse(result)
